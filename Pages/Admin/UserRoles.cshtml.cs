96727e66e0c71ffe7036f87022761f75b7b5e166
Magika34 <maggiana282008@gmail.com>
2026-07-13 20:45:57 +0300

    .

---
+++ Pages/Admin/UserRoles.cshtml.cs
@@ -0,0 +1,94 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GlassyStore.Pages.Admin
+{
+    [Authorize(Roles = "Administrator")]
+    public class UserRolesModel : PageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserRolesModel(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IList<UserRoleViewModel> Users { get; set; } = new List<UserRoleViewModel>();
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var users = _userManager.Users.ToList();
+            var adminRoleExists = await _roleManager.RoleExistsAsync("Administrator");
+
+            foreach (var u in users)
+            {
+                var isAdmin = false;
+                if (adminRoleExists)
+                {
+                    isAdmin = await _userManager.IsInRoleAsync(u, "Administrator");
+                }
+
+                Users.Add(new UserRoleViewModel
+                {
+                    Id = u.Id,
+                    Email = u.Email,
+                    IsAdmin = isAdmin
+                });
+            }
+        }
+
+        public async Task<IActionResult> OnPostAddRoleAsync(string userId)
+        {
+            if (string.IsNullOrEmpty(userId)) return RedirectToPage();
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return NotFound();
+
+            if (!await _roleManager.RoleExistsAsync("Administrator"))
+            {
+                await _roleManager.CreateAsync(new IdentityRole("Administrator"));
+            }
+
+            if (!await _userManager.IsInRoleAsync(user, "Administrator"))
+            {
+                await _userManager.AddToRoleAsync(user, "Administrator");
+                StatusMessage = $"User {user.Email} promoted to Administrator.";
+            }
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostRemoveRoleAsync(string userId)
+        {
+            if (string.IsNullOrEmpty(userId)) return RedirectToPage();
+
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null) return NotFound();
+
+            if (await _userManager.IsInRoleAsync(user, "Administrator"))
+            {
+                await _userManager.RemoveFromRoleAsync(user, "Administrator");
+                StatusMessage = $"User {user.Email} removed from Administrator role.";
+            }
+
+            return RedirectToPage();
+        }
+    }
+
+    public class UserRoleViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
